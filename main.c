9fa59060bbd6a7ffadb548174a10b688d07e50ec
Ryo ONODERA <ryo@tetera.org>
2019-02-27 22:00:44 +0900

    Use statvfs(2) for NetBSD

--- main.c
+++ main.c
@@ -429,17 +429,31 @@ static bool register_roots(array* new_roots, array* unwatchable, array* mounts) 
 #if defined(__BSD__)
 
 static array* unwatchable_mounts() {
+#if defined(__NetBSD__)
+  int fsCount = getvfsstat(NULL, 0, MNT_WAIT);
+#else
   int fsCount = getfsstat(NULL, 0, MNT_WAIT);
+#endif
   if (fsCount > 0) {
+#if defined(__NetBSD__)
+    struct statvfs fs[fsCount];
+    fsCount = getvfsstat(fs, (int)(sizeof(struct statvfs) * fsCount), MNT_NOWAIT);
+#else
     struct statfs fs[fsCount];
     fsCount = getfsstat(fs, (int)(sizeof(struct statfs) * fsCount), MNT_NOWAIT);
+#endif
     if (fsCount > 0) {
       array* mounts = array_create(fsCount);
       CHECK_NULL(mounts, NULL);
 
       for (int i=0; i<fsCount; i++) {
+#if defined(__NetBSD__)
+        userlog(LOG_DEBUG, "mtab: %s : %s (%d)", fs[i].f_mntonname, fs[i].f_fstypename, fs[i].f_flag);
+        if ((fs[i].f_flag & MNT_LOCAL) != MNT_LOCAL) {
+#else
         userlog(LOG_DEBUG, "mtab: %s : %s (%d)", fs[i].f_mntonname, fs[i].f_fstypename, fs[i].f_flags);
         if ((fs[i].f_flags & MNT_LOCAL) != MNT_LOCAL) {
+#endif
           CHECK_NULL(array_push(mounts, strdup(fs[i].f_mntonname)), NULL);
         }
       }
